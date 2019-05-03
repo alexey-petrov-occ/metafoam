e7d7a237da43ff95ed72b81b0c0b35b383b94184
Alexey Petrov <alexey.petrov@opencascade.com>
2019-05-03 08:47:38 +0300

    programming language agnostic support

--- artefacts/models2attributes.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-list_of_models=${1}
-models_source_dir=${2}
-output_dir=${3}
-cat ${list_of_models} | while read model; do find ${models_source_dir} -name ${model}.C -exec artefacts/extract_attributes.sh ${output_dir} {} + ; done
--- artefacts/viscosity/models2attributes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-foam=${1}
-
-dir=$(cd `dirname $0` && pwd)
-source ${dir}/env.sh
-
-artefacts/models2attributes.sh ${viscosity_file}.txt ${transportModels_dir} ${viscosity_dir}
