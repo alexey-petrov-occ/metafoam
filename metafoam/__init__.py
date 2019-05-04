e2c9b6ff11f050b4935930571038a96d18a7e789
Alexey Petrov <alexey.petrov@opencascade.com>
2019-05-04 14:03:36 +0300

    Code refactoring (PyLint & Coverage)

--- metafoam/__init__.py
+++ metafoam/__init__.py
@@ -1,6 +1,14 @@
+"""Defines metamodel for OpenFOAM
+"""
+
 from typing import Dict
 
 JSDocument = Dict
 
 def categories(model: JSDocument) -> Dict:
+    "Extracts 'categories' from the given OpenFOAM 'model' description"
     return dict((item['name'], item['models']) for item in model['categories'])
+
+def models(model: JSDocument) -> Dict:
+    "Extracts 'models' from the given OpenFOAM 'model' description"
+    return dict((item['name'], item['attrs']) for item in model['models'])
