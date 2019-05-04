1ad661aa5d40ed9a8f18206a7570cd2a6d7a66e5
Alexey Petrov <alexey.petrov@opencascade.com>
2019-05-04 08:51:36 +0300

    support Python 'typing' vis 'mypy'

--- test/test_schema.py
+++ test/test_schema.py
@@ -165,29 +165,102 @@ def test_solver():
     instance = {'transport': 'viscosity'}
     validate(instance, solver_schema)
 
-def categories(model):
+from typing import Dict, Set
+
+JSDocument = Dict
+JSSchema = Dict
+Name = str
+
+def categories(model: JSDocument) -> Dict:
     return dict((item['name'], item['models']) for item in model['categories'])
 
-def validate_model(document, schema):
+def validate_model(document: JSDocument, schema: JSSchema) -> None:
     validate(document, schema)
-    category2model = categories(document['transport'])
-    models = set()
-    for value in category2model.values():
+    category2models = categories(document['transport'])
+    models: Set[Name] = set()
+    for value in category2models.values():
         models.update(value)
 
     model2attrs = dict((item['name'], item['attrs']) for item in document['transport']['models'])
     assert set(models) <= set(model2attrs)
 
-def test_show_categories():
+def test_validate_model():
     model = {'transport': {
       'models': [{'name': 'a', 'attrs': ['x', 'y']}, {'name': 'b', 'attrs': ['z']}],
       'categories': [{'name': 'K', 'models': ['b']}, {'name': 'L', 'models': ['a', 'c']}]
     }}
     validate(model, models_schema)
 
-    category2model = categories(model['transport'])
-    assert 'K' in category2model
-    assert 'a' in category2model['L']
+    category2models = categories(model['transport'])
+    assert 'K' in category2models
+    assert 'a' in category2models['L']
 
     with pytest.raises(AssertionError):
       validate_model(model, models_schema)
+
+def validate_solver(solver_document: JSDocument, solver_schema: JSSchema, model_document: JSDocument) -> None:
+    validate(solver_document, solver_schema)
+
+    category2models = categories(model_document['transport'])
+    assert solver_document['transport'] in category2models
+
+def test_validate_solver():
+    model = {'transport': {
+      'models': [{'name': 'a', 'attrs': ['x', 'y']}, {'name': 'b', 'attrs': ['z']}],
+      'categories': [{'name': 'K', 'models': ['b']}, {'name': 'L', 'models': ['a', 'c']}]
+    }}
+
+    solver = {'transport': 'K'}
+    validate_solver(solver, solver_schema, model)
+
+    with pytest.raises(AssertionError):
+      solver = {'transport': 'M'}
+      validate_solver(solver, solver_schema, model)
+
+class Solver(object):
+    __slots__ = ('_solver', '_model', '_transport_model')
+    def __init__(self, solver_document: JSDocument, solver_schema: JSSchema, model_document: JSDocument):
+        validate_solver(solver_document, solver_schema, model_document)
+        self._solver = solver_document
+        self._model = model_document
+        self._transport_model: Name = ''
+
+    @property
+    def transport(self) -> Name:
+        return self._solver['transport']
+
+    @property
+    def transport_model(self) -> Name:
+        return self._transport_model
+
+    @transport_model.setter
+    def transport_model(self, value: Name) -> None:
+        category2models = categories(self._model['transport'])
+        assert value in category2models[self.transport]
+
+        self._transport_model = value
+
+def validate_slots(instance):
+    with pytest.raises(AttributeError):
+      setattr(instance, 'dummy', 1)
+
+    return instance
+
+def test_solver_introspection():
+    model = {'transport': {
+      'models': [{'name': 'a', 'attrs': ['x', 'y']}, {'name': 'b', 'attrs': ['z']}],
+      'categories': [{'name': 'K', 'models': ['b']}, {'name': 'L', 'models': ['a', 'c']}]
+    }}
+
+    solver = {'transport': 'K'}
+
+    inst = validate_slots(Solver(solver, solver_schema, model))
+    assert inst.transport == 'K'
+
+    assert inst.transport_model == ''
+
+    inst.transport_model = 'b'
+    assert inst.transport_model == 'b'
+
+    with pytest.raises(AssertionError):
+        inst.transport_model = 'a'
