815c1f5db481213af24047087dd26c89531b0aef
Alexey Petrov <alexey.petrov@opencascade.com>
2019-05-06 11:35:39 +0300

    properly using jsonschema 'definitions' keyword

--- test/test_solver.py
+++ test/test_solver.py
@@ -1,56 +1,72 @@
 import pytest
 
 import jsonschema as js
+import python_jsonschema_objects as pjs
 
 core_schema = \
 {
-  "defs": {
-    'attr': {
+  "definitions": {
+    "name": {
       "type": "string",
     },
+    'attr': {'type': 'object', 'properties': {
+      'name': {"$ref": "#/definitions/name"},
+      'value': {"oneOf": [{ "type": "string" }, { "type": "number" }]},
+    }},
     "attrs": {
       "type": "array",
       "items": [
         {
-          "$ref": "#/defs/attr"
+          "$ref": "#/definitions/attr"
         }
       ]
     },
     'model': {
       'type': 'object', 'properties': {
         'name': {'type': 'string'},
-        '$ref': '#/defs/attrs'
+        'attrs': {'$ref': '#/definitions/attrs'},
       }
-    },
-    "name": {
-      "type": "string",
     },
     "names": {
       "type": "array",
       "items": [
         {
-          "$ref": "#/defs/name"
+          "$ref": "#/definitions/name"
         }
       ]
     },
     'category': {
       'type': 'object', 'properties': {
         'name': {'type': 'string'},
-        'models': {"$ref": "#/defs/names"}
+        'models': {"$ref": "#/definitions/names"}
       }
     }
   },
-  'type': 'object', 'properties': {
-    'transport': {'type': 'object', 'properties': {
-      'models': {"type": "array", "items": [{"$ref": "#/defs/model"}]},
-      'categories': {"type": "array", "items": [{"$ref": "#/defs/category"}]}
+  'type': 'object', 'title': 'core', 'properties': {
+    'transport': {'type': 'object', 'title': 'transport', 'properties': {
+      'models': {"type": "array", "items": [{"$ref": "#/definitions/model"}]},
+      'categories': {"type": "array", "items": [{"$ref": "#/definitions/category"}]}
     }}
   }
 }
 
 def test_core():
+    with pytest.raises(js.exceptions.ValidationError):
+        instance = {'transport': {
+          'models': [{'name': 'a', 'attrs': ['x', 'y']}, {'name': 'b', 'attrs': ['z']}],
+          'categories': [{'name': 'viscosity', 'models': ['b']}]
+        }}
+        js.validate(instance, core_schema)
+
     instance = {'transport': {
-      'models': [{'name': 'a', 'attrs': ['x', 'y']}, {'name': 'b', 'attrs': ['z']}],
-      'categories': [{'name': 'viscosity', 'models': ['b']}]
+      'models': [{'name': 'a', 'attrs': [{'name': 'x', 'value': 1}, {'name': 'y', 'value': 'abc'}]}]
     }}
     js.validate(instance, core_schema)
+
+    builder = pjs.ObjectBuilder(core_schema)
+    ns = builder.build_classes()
+
+    attr = ns.Attr()
+    attr.value = 1
+    # with pytest.raises(pjs.validators.ValidationError):
+    #   attr.value = ''
