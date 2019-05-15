d12908e31b1da3be147bfbc9a34f9078abcef4a8
Telefon2232 <telefon2232@gmail.com>
2019-05-15 21:04:06 +0300

    add func

---
+++ json_format.py
@@ -0,0 +1,11 @@
+def handler_json():
+    source = {'a': ['x', 'y'], 'b': ['z'], 'c': []}
+    mas=[]
+    print(list(source.keys()))
+    for key in source.keys():
+        mas.append({'name':key,'attrs':source.get(key)})
+    categories=[{'name':'viscosity','models':list(source.keys())}]
+    models={'models':mas,'categories':categories}
+    main={'transport':models}
+    print(main)
+    return main
