115a9185f276708f35fcac3c3f35b7bc9093eea4
Alexey Petrov <alexey.petrov@opencascade.com>
2019-05-18 12:27:17 +0300

    advancing 'core schema' implementation

--- test/test_schema-2.py
+++ test/test_schema-2.py
@@ -12,23 +12,77 @@ def core_schema():
                 'value': {'type': 'number'},
                 'name': {'type': 'string'},
             }, 'required': ['name'], 'additionalProperties': False},
-        },
-        'title': 'test', 'type': 'object', 'properties': {
-            'x_attr': {'$ref': '#/definitions/x-type'},
+            'x-attr': {'title': 'XAttr', 'type': 'object', 'properties': {
+              'x_attr': {'$ref': '#/definitions/x-type'},
+            }, 'required': ['x_attr'], 'additionalProperties': False},
+            'y-type': {'title': 'YType', 'type': 'object', 'properties': {
+                'value': {'type': 'string'},
+                'name': {'type': 'string'},
+            }, 'required': ['name'], 'additionalProperties': False},
+            'y-attr': {'title': 'YAttr', 'type': 'object', 'properties': {
+              'y_attr': {'$ref': '#/definitions/y-type'},
+            }, 'required': ['y_attr'], 'additionalProperties': False},
+            'attrs': {'title': 'TAttr', 'type': 'array', 'items': {'oneOf': [
+                {'$ref': '#/definitions/x-attr'},
+                {'$ref': '#/definitions/y-attr'},
+            ]}, 'additionalItems': False, 'uniqueItems': True},
         },
     }
 
 
-def test(core_schema):
-    js.validate({'x_attr': {'name': 'x', 'value': 1}}, core_schema)
+def validate(document, schema):
+    test = {'test': document}
+    js.validate(test, schema)
 
-    js.validate({'x_attr': {'name': 'x'}}, core_schema)
+
+def test_attrs(core_schema):
+    test_schema = {
+        'title': 'test', 'type': 'object', 'properties': {
+            'test': {'$ref': '#/definitions/attrs'},
+        },
+    }
+    core_schema.update(test_schema)
+
+    document = [
+        {'x_attr': {'name': 'x', 'value': 1}},
+        {'y_attr': {'name': 'y', 'value': '1'}},
+    ]
+    validate(document, core_schema)
 
     with pytest.raises(js.exceptions.ValidationError):
-        js.validate({'x_attr': {'value': 1}}, core_schema)
+        document = [
+            {'x_attr': {'name': 'x', 'value': 1}},
+            {'x_attr': {'name': 'x', 'value': 1}},
+        ]
+        validate(document, core_schema)  # check on 'uniqueItems'
+
+    document = [
+        {'y_attr': {'name': 'y', 'value': '1'}},
+        {'y_attr': {'name': 'y', 'value': '2'}},
+    ]
+    validate(document, core_schema)  # check on 'uniqueItems'
 
     with pytest.raises(js.exceptions.ValidationError):
-        js.validate({'x_attr': {}}, core_schema)
+        validate([{'a': 'b'}], core_schema)  # check on 'additionalItems'
+
+
+def test_x_attr(core_schema):
+    test_schema = {
+        'title': 'test', 'type': 'object', 'properties': {
+            'test': {'$ref': '#/definitions/x-attr'},
+        },
+    }
+    core_schema.update(test_schema)
+
+    validate({'x_attr': {'name': 'x', 'value': 1}}, core_schema)
+
+    validate({'x_attr': {'name': 'x'}}, core_schema)
+
+    with pytest.raises(js.exceptions.ValidationError):
+        validate({'x_attr': {'value': 1}}, core_schema)
+
+    with pytest.raises(js.exceptions.ValidationError):
+        validate({'x_attr': {}}, core_schema)
 
     builder = pjs.ObjectBuilder(core_schema)
     ns = builder.build_classes(strict=True, named_only=False, standardize_names=False)
