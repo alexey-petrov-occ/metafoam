c4a3fbcd39f08c0f807f163c55f0e951a7a9e9fd
Alexey Petrov <alexey.petrov@opencascade.com>
2019-05-18 14:06:22 +0300

    'models' & 'categories'

--- test/test_schema-2.py
+++ test/test_schema-2.py
@@ -7,7 +7,7 @@ import python_jsonschema_objects as pjs
 @pytest.fixture
 def core_schema():
     return {
-        "definitions": {
+        'definitions': {
             'x-type': {'title': 'XType', 'type': 'object', 'properties': {
                 'value': {'type': 'number'},
                 'name': {'type': 'string'},
@@ -22,17 +22,143 @@ def core_schema():
             'y-attr': {'title': 'YAttr', 'type': 'object', 'properties': {
               'y_attr': {'$ref': '#/definitions/y-type'},
             }, 'required': ['y_attr'], 'additionalProperties': False},
-            'attrs': {'title': 'TAttr', 'type': 'array', 'items': {'oneOf': [
+            'attrs': {'title': 'TAttrs', 'type': 'array', 'items': {'oneOf': [
                 {'$ref': '#/definitions/x-attr'},
                 {'$ref': '#/definitions/y-attr'},
             ]}, 'additionalItems': False, 'uniqueItems': True},
-        },
+            'model': {'type': 'object', 'properties': {
+                'name': {'type': 'string'},
+                'attrs': {'$ref': '#/definitions/attrs'},
+            }, 'required': ['name'], 'additionalProperties': False},
+            'models': {'type': 'array', 'items': [
+                {'$ref': '#/definitions/model'},
+            ], 'additionalItems': False, 'uniqueItems': True},
+            'names': {'type': 'array', 'items': {'oneOf': [
+                {'type': 'string'},
+            ]}, 'additionalItems': False, 'uniqueItems': True},
+            'category': {'type': 'object', 'properties': {
+                'name': {'type': 'string'},
+                'models': {'$ref': '#/definitions/names'}
+            }, 'required': ['name', 'models'], 'additionalProperties': False},
+            'categories': {'type': 'array', 'items': {'oneOf': [
+                {'$ref': '#/definitions/category'}
+            ]}, 'additionalItems': False, 'uniqueItems': True}
+         },
     }
 
 
 def validate(document, schema):
     test = {'test': document}
     js.validate(test, schema)
+
+
+def update_schema(schema, entity):
+    test_schema = {
+        'title': 'test', 'type': 'object', 'properties': {
+            'test': {'$ref': '#/definitions/{}'.format(entity)},
+        },
+    }
+    schema.update(test_schema)
+    return schema
+
+
+def test_categories(core_schema):
+    update_schema(core_schema, 'categories')
+
+    validate([], core_schema)
+    validate([{'name': 'K', 'models': []}], core_schema)
+
+    with pytest.raises(js.exceptions.ValidationError):
+        document = [
+            {'name': 'K', 'models': []},
+            {'a': 'b'},
+        ]
+        validate(document, core_schema)  # check on 'additionalItems'
+
+    with pytest.raises(js.exceptions.ValidationError):
+        document = [
+            {'name': 'K', 'models': []},
+            {'name': 'K', 'models': []},
+        ]
+        validate(document, core_schema)  # check on 'uniqueItems'
+
+        document = [  # but it does not guaranty uniquenesses in the following case
+            {'name': 'K', 'models': []},
+            {'name': 'K', 'models': ['A', 'B']},
+        ]
+
+
+def test_category(core_schema):
+    update_schema(core_schema, 'category')
+
+    validate({'name': 'K', 'models': []}, core_schema)
+    validate({'name': 'K', 'models': ['A', 'B']}, core_schema)
+
+    with pytest.raises(js.exceptions.ValidationError):
+        validate({}, core_schema)  # check on 'required'
+
+    with pytest.raises(js.exceptions.ValidationError):
+        document = {'name': 'K', 'models': [], 'a': 'b'}
+        validate(document, core_schema)  # check on 'additionalProperties'
+
+
+def test_names(core_schema):
+    update_schema(core_schema, 'names')
+
+    validate([], core_schema)
+    validate(['A'], core_schema)
+    validate(['A', 'B'], core_schema)
+
+    with pytest.raises(js.exceptions.ValidationError):
+        validate([1], core_schema)  # check on 'additionalItems'
+
+    with pytest.raises(js.exceptions.ValidationError):
+        validate(['A', 1], core_schema)  # check on 'additionalItems'
+
+    with pytest.raises(js.exceptions.ValidationError):
+        validate(['A', 'A'], core_schema)  # check on 'uniqueItems'
+
+
+def test_models(core_schema):
+    update_schema(core_schema, 'models')
+
+    document = [{'name': 'A'}]
+    validate(document, core_schema)
+
+    with pytest.raises(js.exceptions.ValidationError):
+        document = {'a': 'b'}
+        validate(document, core_schema)  # check on 'additionalItems'
+
+    with pytest.raises(js.exceptions.ValidationError):
+        document = [
+            {'name': 'A', 'attrs': [{'x_attr': {'name': 'x', 'value': 1}}]},
+            {'name': 'A'},
+        ]
+        validate(document, core_schema)  # check on 'uniqueItems'
+
+
+def test_model(core_schema):
+    update_schema(core_schema, 'model')
+
+    document = {'name': 'A'}
+    validate(document, core_schema)
+
+    document = {'name': 'A', 'attrs': []}
+    validate(document, core_schema)
+
+    with pytest.raises(js.exceptions.ValidationError):
+        document = {'attrs': []}
+        validate(document, core_schema)  # check on 'required'
+
+    with pytest.raises(js.exceptions.ValidationError):
+        document = {'a': 'b'}
+        validate(document, core_schema)  # check on 'additionalItems'
+
+    document = {'name': 'A', 'attrs': [
+        {'x_attr': {'name': 'x', 'value': 1}},
+        {'y_attr': {'name': 'y', 'value': '1'}},
+    ]}
+    validate(document, core_schema)
 
 
 def test_attrs(core_schema):
