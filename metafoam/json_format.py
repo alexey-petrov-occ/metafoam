98b0c6b0350567f75a843c5c21ab0a3c04f00650
telefon2232 <telefon2232@gmail.com>
2019-05-20 11:29:36 +0300

    update json_format

--- metafoam/json_format.py
+++ metafoam/json_format.py
@@ -1,12 +1,17 @@
-source = {'a': ['x', 'y'], 'b': ['z'], 'c': []}
+"""
+create json format
+"""
+Source = {'a': ['x', 'y'], 'b': ['z'], 'c': []}
+
+
 def handler_json(source):
-    
-    mas=[]
+    """print and return json format"""
+    mas = []
     print(list(source.keys()))
     for key in source.keys():
-        mas.append({'name':key,'attrs':source.get(key)})
-    categories=[{'name':'viscosity','models':list(source.keys())}]
-    models={'models':mas,'categories':categories}
-    main={'transport':models}
+        mas.append({'name': key, 'attrs': source.get(key)})
+    categories = [{'name': 'viscosity', 'models': list(source.keys())}]
+    models = {'models': mas, 'categories': categories}
+    main = {'transport': models}
     print(main)
     return main
